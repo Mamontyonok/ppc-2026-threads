656a6dfc4589a160bb7e7bb9b6df630ce67ed822
Mamontyonok <kulikartur05@yandex.ru>
2026-04-18 14:22:00 +0300

    logic_complexity

--- tasks/kulik_a_mat_mul_double_ccs/tbb/src/ops_tbb.cpp
+++ tasks/kulik_a_mat_mul_double_ccs/tbb/src/ops_tbb.cpp
@@ -14,6 +14,52 @@
 
 namespace kulik_a_mat_mul_double_ccs {
 
+namespace {
+
+inline void ProcessColumn(size_t j, const CCS &a, const CCS &b, std::vector<double> &accum,
+                          std::vector<bool> &nz_elem_rows, std::vector<size_t> &nnz_rows,
+                          std::vector<std::vector<double>> &local_values,
+                          std::vector<std::vector<size_t>> &local_rows) {
+  for (size_t k = b.col_ind[j]; k < b.col_ind[j + 1]; ++k) {
+    size_t ind = b.row[k];
+    double b_val = b.value[k];
+    for (size_t zc = a.col_ind[ind]; zc < a.col_ind[ind + 1]; ++zc) {
+      size_t i = a.row[zc];
+      double a_val = a.value[zc];
+
+      accum[i] += a_val * b_val;
+      if (!nz_elem_rows[i]) {
+        nz_elem_rows[i] = true;
+        nnz_rows.push_back(i);
+      }
+    }
+  }
+
+  std::ranges::sort(nnz_rows);
+
+  for (size_t i : nnz_rows) {
+    if (accum[i] != 0.0) {
+      local_rows[j].push_back(i);
+      local_values[j].push_back(accum[i]);
+    }
+    accum[i] = 0.0;
+    nz_elem_rows[i] = false;
+  }
+  nnz_rows.clear();
+}
+
+inline void CopyColumn(size_t j, CCS &c, const std::vector<std::vector<double>> &local_values,
+                       const std::vector<std::vector<size_t>> &local_rows) {
+  size_t offset = c.col_ind[j];
+  size_t col_nz = local_values[j].size();
+  for (size_t k = 0; k < col_nz; ++k) {
+    c.value[offset + k] = local_values[j][k];
+    c.row[offset + k] = local_rows[j][k];
+  }
+}
+
+}  // namespace
+
 struct ThreadLocalData {
   std::vector<double> accum;
   std::vector<bool> nz_elem_rows;
@@ -55,37 +101,9 @@ bool KulikAMatMulDoubleCcsTBB::RunImpl() {
 
   tbb::parallel_for(tbb::blocked_range<size_t>(0, b.m), [&](const tbb::blocked_range<size_t> &r) {
     auto &t_data = tls.local();
-    auto &accum = t_data.accum;
-    auto &nz_elem_rows = t_data.nz_elem_rows;
-    auto &nnz_rows = t_data.nnz_rows;
 
     for (size_t j = r.begin(); j != r.end(); ++j) {
-      for (size_t k = b.col_ind[j]; k < b.col_ind[j + 1]; ++k) {
-        size_t ind = b.row[k];
-        double b_val = b.value[k];
-        for (size_t zc = a.col_ind[ind]; zc < a.col_ind[ind + 1]; ++zc) {
-          size_t i = a.row[zc];
-          double a_val = a.value[zc];
-
-          accum[i] += a_val * b_val;
-          if (!nz_elem_rows[i]) {
-            nz_elem_rows[i] = true;
-            nnz_rows.push_back(i);
-          }
-        }
-      }
-
-      std::ranges::sort(nnz_rows);
-
-      for (size_t i : nnz_rows) {
-        if (accum[i] != 0.0) {
-          local_rows[j].push_back(i);
-          local_values[j].push_back(accum[i]);
-        }
-        accum[i] = 0.0;
-        nz_elem_rows[i] = false;
-      }
-      nnz_rows.clear();
+      ProcessColumn(j, a, b, t_data.accum, t_data.nz_elem_rows, t_data.nnz_rows, local_values, local_rows);
     }
   });
 
@@ -102,12 +120,7 @@ bool KulikAMatMulDoubleCcsTBB::RunImpl() {
 
   tbb::parallel_for(tbb::blocked_range<size_t>(0, b.m), [&](const tbb::blocked_range<size_t> &r) {
     for (size_t j = r.begin(); j != r.end(); ++j) {
-      size_t offset = c.col_ind[j];
-      size_t col_nz = local_values[j].size();
-      for (size_t k = 0; k < col_nz; ++k) {
-        c.value[offset + k] = local_values[j][k];
-        c.row[offset + k] = local_rows[j][k];
-      }
+      CopyColumn(j, c, local_values, local_rows);
     }
   });
 
